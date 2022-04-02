90555ed2808d6f8984566f414d8ef8964113ac1d
cxz <1838038611@qq.com>
2022-04-02 09:08:35 +0800

    update

--- Text-Classification/RNN/RNN.py
+++ Text-Classification/RNN/RNN.py
@@ -4,9 +4,17 @@ import torch.nn.functional as F
 import numpy as np
 
 
-class Config():
+class Config(object):
     def __init__(self, dataset):
-        pass
+        self.model_name = 'RNN'
+        self.train_path = dataset + '/data/train.txt'  # 训练集
+        self.dev_path = dataset + '/data/dev.txt'  # 验证集
+        self.test_path = dataset + '/data/test.txt'  # 测试集
+        self.class_list = [x.strip() for x in open(
+            dataset + '/data/class.txt', encoding='utf-8').readlines()]  # 类别名单
+        self.save_path = dataset + '/saved_dict/' + self.model_name + '.ckpt'  # 模型训练结果
+        self.log_path = dataset + '/log/' + self.model_name
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # 设备
 
 
 class Model(nn.Module):
