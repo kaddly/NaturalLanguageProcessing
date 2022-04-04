b447e814a54f857a4414364d2e325a133b7a1303
cxz <1838038611@qq.com>
2022-04-04 11:07:33 +0800

    update

---
+++ SequenceForcecast/RNN/run.py
@@ -0,0 +1,8 @@
+from RNN import *
+from utils import *
+from train_eval import *
+import torch
+import torch.nn as nn
+
+if __name__ == '__main__':
+    pass
--- SequenceForcecast/RNN/train_eval.py
+++ SequenceForcecast/RNN/train_eval.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import time
+import math
 
 
 def grad_clipping(net, theta):  # @save
@@ -29,4 +31,52 @@ def predict_seq(prefix, num_preds, net, vocab, device):
 
 
 def train_epoch_seq(net, train_iter, loss, updater, device, use_random_iter):
-    pass
+    state = None
+    tik = time.time()
+    for X, Y in train_iter:
+        if state is None or use_random_iter:
+            # 在第一次使用随机抽样时初始化状态
+            state = net.begin_state(batch_size=X.shape[0], device=device)
+        else:
+            if isinstance(net, nn.Module) and not isinstance(state, tuple):
+                # state 对于GRU是个张量
+                state.detach_()
+            else:
+                # state对于nn.LSTM或对于我们从零开始实现的模型是个张量
+                for s in state:
+                    s.detach_
+        y = Y.T.reshape(-1)
+        X, y = X.to(device), y.to(device)
+        y_hat, state = net(X, state)
+        l = loss(y_hat, y.long()).mean()
+        if isinstance(updater, torch.optim.Optimizer):
+            updater.zero_grad()
+            l.backward()
+            grad_clipping(net, 1)
+            updater.step()
+        else:
+            l.backward()
+            grad_clipping(net, 1)
+            # 因为已经调⽤了mean函数
+            updater(batch_size=1)
+    return math.exp(l * y.numel() / y.numel()), y.numel() / (time.time() - tik)
+
+
+def train_seq(net, train_iter, vocab, lr, num_epochs, device, use_random_iter=False):
+    """训练模型（定义⻅第8章）"""
+    loss = nn.CrossEntropyLoss()
+    # 初始化
+    if isinstance(net, nn.Module):
+        updater = torch.optim.SGD(net.parameters(), lr)
+    else:
+        updater = None
+    predict = lambda prefix: predict_seq(prefix, 50, net, vocab, device)
+    # 训练和预测
+    for epoch in range(num_epochs):
+        ppl, speed = train_epoch_seq(net, train_iter, loss, updater, device, use_random_iter)
+        if (epoch + 1) % 10 == 0:
+            print("epoch:" + str((epoch + 1)) + f'困惑度 {ppl:.1f}')
+            print(predict('time traveller'))
+    print(f'困惑度 {ppl:.1f}, {speed:.1f} 词元/秒 {str(device)}')
+    print(predict('time traveller'))
+    print(predict('traveller'))
