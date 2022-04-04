b26ed38e581f57847973c12854736a8ee192b8f9
cxz <1838038611@qq.com>
2022-04-04 10:32:52 +0800

    update

--- SequenceForcecast/RNN/train_eval.py
+++ SequenceForcecast/RNN/train_eval.py
@@ -12,3 +12,21 @@ def grad_clipping(net, theta):  # @save
     if norm > theta:
         for param in params:
             param.grad[:] *= theta / norm
+
+
+def predict_seq(prefix, num_preds, net, vocab, device):
+    """在prefix后⾯⽣成新字符"""
+    state = net.begin_state(batch_size=1, device=device)
+    outputs = [vocab[prefix[0]]]
+    get_input = lambda: torch.tensor([outputs[-1]], device=device).reshape((1, 1))
+    for y in prefix[1:]:  # 预热期
+        _, state = net(get_input(), state)
+        outputs.append(vocab[y])
+    for _ in range(num_preds):  # 预测num_preds步
+        y, state = net(get_input(), state)
+        outputs.append(int(y.argmax(dim=1).reshape(1)))
+    return ''.join([vocab.idx_to_token[i] for i in outputs])
+
+
+def train_epoch_seq(net, train_iter, loss, updater, device, use_random_iter):
+    pass
