482d35801088c58808ffd801493efdaec8fc16d0
cxz <1838038611@qq.com>
2022-04-05 11:02:51 +0800

    update

--- SequenceForcecast/GRU/GRU.py
+++ SequenceForcecast/GRU/GRU.py
@@ -35,3 +35,45 @@ class RNNModel(nn.Module):
             return (
                 torch.zeros((self.num_directions * self.rnn.num_layers, batch_size, self.num_hiddens), device=device),
                 torch.zeros((self.num_directions * self.rnn.num_layers, batch_size, self.num_hiddens), device=device))
+
+
+def getParams(vocab_size, num_hiddens, device):
+    num_inputs = num_output = vocab_size
+
+    def normal(shape):
+        return torch.randn(size=shape, device=device) * 0.01
+
+    def tree():
+        return (
+            normal((num_inputs, num_hiddens)), normal((num_hiddens, num_hiddens)),
+            torch.zeros(num_hiddens, device=device))
+
+    W_xz, W_hz, b_z = tree()  # 更新门参数
+    W_xr, W_hr, b_r = tree()  # 重置门参数
+    W_xh, W_hh, b_h = tree()  # 候选隐藏门参数
+    # 输出层参数
+    W_hq = normal((num_hiddens, num_output))
+    b_q = torch.zeros(size=num_hiddens, device=device)
+    params = [W_xz, W_hz, b_z, W_xr, W_hr, b_r, W_xh, W_hh, b_h, W_hq, b_q]
+    for param in params:
+        param.requires_grad_(True)
+    return params
+
+
+def init_gru_state(batch_size, num_hiddens, device):
+    return (torch.zeros(size=(batch_size, num_hiddens), device=device),)
+
+
+def gru(inputs, state, params):
+    W_xz, W_hz, b_z, W_xr, W_hr, b_r, W_xh, W_hh, b_h, W_hq, b_q = params
+    H, = state
+
+    outputs = []
+    for X in inputs:
+        Z = torch.sigmoid((X @ W_xz) + (H @ W_hz) + b_z)
+        R = torch.sigmoid((X @ W_xr) + (H @ W_hr) + b_r)
+        H_tilda = torch.tanh((X @ W_xh) + (R * H) @ W_hh + b_h)
+        H = Z * H + (1 - Z) * H_tilda
+        Y = H @ W_hq + b_q
+        outputs.append(Y)
+    return torch.cat(outputs, dim=0), (H,)
