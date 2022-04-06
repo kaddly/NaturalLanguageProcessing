75c2059a0d2dda17d13fccf670e93644d355b8c1
cxz <1838038611@qq.com>
2022-04-06 10:47:11 +0800

    update

--- SequenceForcecast/LSTM/run.py
+++ SequenceForcecast/LSTM/run.py
@@ -5,12 +5,23 @@ import torch
 import torch.nn as nn
 
 if __name__ == '__main__':
+    # batch_size, num_steps = 32, 35
+    # num_epochs, lr = 500, 1
+    # train_iter, vocab = load_data_time_machine(batch_size, num_steps)
+    # num_hiddens = 256
+    # lstm_layer = nn.LSTM(len(vocab), num_hiddens)
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # net = RNNModel(lstm_layer, vocab_size=len(vocab))
+    # net = net.to(device)
+    # train_seq(net, train_iter, vocab, lr, num_epochs, device)
+
     batch_size, num_steps = 32, 35
-    num_epochs, lr = 500, 1
     train_iter, vocab = load_data_time_machine(batch_size, num_steps)
-    num_hiddens = 256
-    lstm_layer = nn.LSTM(len(vocab), num_hiddens)
+    vocab_size, num_hiddens, num_layers = len(vocab), 256, 2
+    num_inputs = vocab_size
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    net = RNNModel(lstm_layer, vocab_size=len(vocab))
-    net = net.to(device)
-    train_seq(net, train_iter, vocab, lr, num_epochs, device)
+    lstm_layer = nn.LSTM(num_inputs, num_hiddens, num_layers)
+    model = RNNModel(lstm_layer, len(vocab))
+    model = model.to(device)
+    num_epochs, lr = 500, 2
+    train_seq(model, train_iter, vocab, lr, num_epochs, device)
