3f23d5591fbdb39cb74088c3af785bc2f3ecbbee
cxz <1838038611@qq.com>
2022-04-07 20:18:11 +0800

    update

--- SequenceForcecast/LSTM/run.py
+++ SequenceForcecast/LSTM/run.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 
 if __name__ == '__main__':
+    '''lstm网络'''
     # batch_size, num_steps = 32, 35
     # num_epochs, lr = 500, 1
     # train_iter, vocab = load_data_time_machine(batch_size, num_steps)
@@ -14,7 +15,7 @@ if __name__ == '__main__':
     # net = RNNModel(lstm_layer, vocab_size=len(vocab))
     # net = net.to(device)
     # train_seq(net, train_iter, vocab, lr, num_epochs, device)
-    # 深度循环神经网络
+    '''深度循环神经网络'''
     batch_size, num_steps = 32, 35
     train_iter, vocab = load_data_time_machine(batch_size, num_steps)
     vocab_size, num_hiddens, num_layers = len(vocab), 256, 2
