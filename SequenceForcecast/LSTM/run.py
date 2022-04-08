558d973df5b7a73d6e64e659fe4475b9bd70eb98
cxz <1838038611@qq.com>
2022-04-08 09:56:29 +0800

    update

--- SequenceForcecast/LSTM/run.py
+++ SequenceForcecast/LSTM/run.py
@@ -16,12 +16,23 @@ if __name__ == '__main__':
     # net = net.to(device)
     # train_seq(net, train_iter, vocab, lr, num_epochs, device)
     '''深度循环神经网络'''
+    # batch_size, num_steps = 32, 35
+    # train_iter, vocab = load_data_time_machine(batch_size, num_steps)
+    # vocab_size, num_hiddens, num_layers = len(vocab), 256, 2
+    # num_inputs = vocab_size
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # lstm_layer = nn.LSTM(num_inputs, num_hiddens, num_layers)
+    # model = RNNModel(lstm_layer, len(vocab))
+    # model = model.to(device)
+    # num_epochs, lr = 500, 2
+    # train_seq(model, train_iter, vocab, lr, num_epochs, device)
+    '''双向循环神经网络'''
     batch_size, num_steps = 32, 35
     train_iter, vocab = load_data_time_machine(batch_size, num_steps)
     vocab_size, num_hiddens, num_layers = len(vocab), 256, 2
     num_inputs = vocab_size
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    lstm_layer = nn.LSTM(num_inputs, num_hiddens, num_layers)
+    lstm_layer = nn.LSTM(num_inputs, num_hiddens, num_layers, bidirectional=True)
     model = RNNModel(lstm_layer, len(vocab))
     model = model.to(device)
     num_epochs, lr = 500, 2
