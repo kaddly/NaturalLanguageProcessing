a7cb6d81c5308bc41160889951f5806a7d369f29
cxz <1838038611@qq.com>
2022-04-10 10:51:16 +0800

    update

--- MachineTranslation/Seq2seq/run.py
+++ MachineTranslation/Seq2seq/run.py
@@ -1,5 +1,18 @@
 import torch
 from torch.utils.data import DataLoader
+from Seq2seq import Seq2SeqEncoder, Seq2SeqDecoder, EncoderDecoder
+from utils import My_Dataset
+from train_eval import train_seq2seq
 
 if __name__ == '__main__':
-    pass
+    embed_size, num_hiddens, num_layers, dropout = 32, 32, 2, 0.1
+    batch_size, num_steps = 64, 10
+    lr, num_epochs, device = 0.005, 300, torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    my_dataset = My_Dataset(num_steps=num_steps)
+    src_vocab, tgt_vocab = my_dataset.get_vocab()
+    train_iter = DataLoader(my_dataset, batch_size=batch_size)
+    encoder = Seq2SeqEncoder(len(src_vocab), embed_size, num_hiddens, num_layers, dropout)
+    decoder = Seq2SeqDecoder(len(tgt_vocab), embed_size, num_hiddens, num_layers, dropout)
+    net = EncoderDecoder(encoder, decoder)
+    train_seq2seq(net, train_iter, lr, num_epochs, tgt_vocab, device)
+
--- MachineTranslation/Seq2seq/train_eval.py
+++ MachineTranslation/Seq2seq/train_eval.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import time
 
 
 def sequence_mask(X, valid_len, value=0):
@@ -20,6 +21,55 @@ class MaskedSoftmaxCELoss(nn.CrossEntropyLoss):
         weights = torch.ones_like(label)
         weights = sequence_mask(weights, valid_len)
         self.reduction = 'none'
-        unweighted_loss = super(MaskedSoftmaxCELoss, self).forward(pred.permut(0, 2, 1), label)
+        unweighted_loss = super(MaskedSoftmaxCELoss, self).forward(pred.permute(0, 2, 1), label)
         weighted_loss = (unweighted_loss * weights).mean(dim=1)
         return weighted_loss
+
+
+def grad_clipping(net, theta):  # @save
+    """裁剪梯度"""
+    if isinstance(net, nn.Module):
+        params = [p for p in net.parameters() if p.requires_grad]
+    else:
+        params = net.params
+    norm = torch.sqrt(sum(torch.sum((p.grad ** 2)) for p in params))
+    if norm > theta:
+        for param in params:
+            param.grad[:] *= theta / norm
+
+
+def train_seq2seq(net, data_iter, lr, num_epochs, tgt_vocab, device):
+    """训练序列到序列模型"""
+
+    def xavier_init_weights(m):
+        if type(m) == nn.Linear:
+            nn.init.xavier_uniform_(m.weight)
+        if type(m) == nn.GRU:
+            for param in m._flat_weights_names:
+                if 'weight' in param:
+                    nn.init.xavier_uniform_(m._parameters[param])
+
+    net.apply(xavier_init_weights)
+    net.to(device)
+    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+    loss = MaskedSoftmaxCELoss()
+    net.train()
+    for epoch in range(num_epochs):
+        tik = time.time()
+        for batch in data_iter:
+            optimizer.zero_grad()
+            X, X_valid_len, Y, Y_valid_len = [x.to(device) for x in batch]
+            bos = torch.tensor([tgt_vocab['<bos>']] * Y.shape[0], device=device).reshape(-1, 1)
+            dec_input = torch.cat([bos, Y[:, :-1]], 1)  # 强制教学
+            Y_hat, _ = net(X, dec_input, X_valid_len)
+            l = loss(Y_hat, Y, Y_valid_len)
+            l.sum().backward()
+            grad_clipping(net, 1)
+            num_tokens = Y_valid_len.sum()
+            optimizer.step()
+            with torch.no_grad():
+                l_sum = l.sum()
+        if (epoch + 1) % 10 == 0:
+            print(f"epoch>>{(epoch + 1)}:loss>>{l_sum / num_tokens:.3f}")
+    print(f'loss {l_sum / num_tokens:.3f}, {num_tokens / (time.time() - tik):.1f} '
+          f'tokens/sec on {str(device)}')
