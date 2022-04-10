148d7ad4cddc343d7a751f2152f4c082d5748379
cxz <1838038611@qq.com>
2022-04-10 10:11:49 +0800

    update

--- MachineTranslation/Seq2seq/Seq2seq.py
+++ MachineTranslation/Seq2seq/Seq2seq.py
@@ -1,3 +1,5 @@
+import torch
+
 import torch.nn as nn
 
 
@@ -39,8 +41,48 @@ class EncoderDecoder(nn.Module):
 
 
 class Seq2SeqEncoder(Encoder):
-    def __init__(self):
-        super(Seq2SeqEncoder, self).__init__()
+    """⽤于序列到序列学习的循环神经⽹络编码器"""
 
-    def forward(self):
-        pass
+    def __init__(self, vocab_size, embed_size, num_hiddens, num_layers, dropout=0, **kwargs):
+        super(Seq2SeqEncoder, self).__init__(**kwargs)
+        # 嵌入层
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.rnn = nn.GRU(embed_size, num_hiddens, num_layers, dropout=dropout)
+
+    def forward(self, X, *args):
+        # 输出'X'的形状：(batch_size,num_steps,embed_size)
+        X = self.embedding(X)
+        # 在循环神经⽹络模型中，第⼀个轴对应于时间步
+        X = X.permute(1, 0, 2)
+        # 如果未提及状态，则默认为0
+        output, state = self.rnn(X)
+        # output的形状:(num_steps,batch_size,num_hiddens)
+        # state[0]的形状:(num_layers,batch_size,num_hiddens)
+        return output, state
+
+
+class Seq2SeqDecoder(Decoder):
+    """⽤于序列到序列学习的循环神经⽹络解码器"""
+
+    def __init__(self, vocab_size, embed_size, num_hiddens, num_layers, dropout=0, **kwargs):
+        super(Seq2SeqDecoder, self).__init__(**kwargs)
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.rnn = nn.GRU(embed_size + num_hiddens, num_hiddens, num_layers, dropout=dropout)
+        self.dense = nn.Linear(num_hiddens, vocab_size)
+
+    def init_state(self, enc_outputs, *args):
+        return enc_outputs[1]
+
+    def forward(self, X, state):
+        # 输出'X'的形状：(num_steps,batch_size,embed_size)
+        X = self.embedding(X).permute(1, 0, 2)
+        # ⼴播context，使其具有与X相同的num_steps
+        context = state[-1].repeat(X.shape[0], 1, 1)
+        X_and_context = torch.cat((X, context), 2)
+        output, state = self.rnn(X_and_context, state)
+        output = self.dense(output).permute(1, 0, 2)
+        # output的形状:(batch_size,num_steps,vocab_size)
+        # state[0]的形状:(num_layers,batch_size,num_hiddens)
+        return output, state
+
+
