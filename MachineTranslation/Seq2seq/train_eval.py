8bf1f110cd7c6c12aed0b900c4a91b29b6b2921b
cxz <1838038611@qq.com>
2022-04-11 10:33:31 +0800

    update

--- MachineTranslation/Seq2seq/train_eval.py
+++ MachineTranslation/Seq2seq/train_eval.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import time
+import math
+import collections
 
 
 def sequence_mask(X, valid_len, value=0):
@@ -111,5 +113,16 @@ def predict_seq2seq(net, src_sentence, src_vocab, tgt_vocab, num_steps,
 
 def bleu(pred_seq, label_seq, k):
     """计算BLEU"""
-    
-    pass
+    pred_tokens, label_tokens = pred_seq.split(' '), label_seq.split(' ')
+    len_pred, len_label = len(pred_tokens), len(label_tokens)
+    score = math.exp(min(0, 1 - len_pred / len_label))  # 惩罚过短的预测
+    for n in range(1, k + 1):
+        num_matches, label_subs = 0, collections.defaultdict(int)
+        for i in range(len_label - n + 1):
+            label_subs[' '.join(label_tokens[i:i + n])] += 1
+        for i in range(len_pred - n + 1):
+            if label_subs[' '.join(pred_tokens[i: i + n])] > 0:
+                num_matches += 1
+                label_subs[' '.join(pred_tokens[i: i + n])] -= 1
+        score *= math.pow(num_matches / (len_pred - n + 1), math.pow(0.5, n))
+    return score
