e5e3029b935d4b7f2c55a5b9c356b76a741de4ae
cxz <1838038611@qq.com>
2022-04-11 10:39:02 +0800

    update

--- MachineTranslation/Seq2seq/run.py
+++ MachineTranslation/Seq2seq/run.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import DataLoader
 from Seq2seq import Seq2SeqEncoder, Seq2SeqDecoder, EncoderDecoder
 from utils import My_Dataset
-from train_eval import train_seq2seq
+from train_eval import train_seq2seq, predict_seq2seq, bleu
 
 if __name__ == '__main__':
     embed_size, num_hiddens, num_layers, dropout = 32, 32, 2, 0.1
@@ -16,3 +16,10 @@ if __name__ == '__main__':
     net = EncoderDecoder(encoder, decoder)
     train_seq2seq(net, train_iter, lr, num_epochs, tgt_vocab, device)
 
+    # 测试模型
+    engs = ['go .', "i lost .", 'he\'s calm .', 'i\'m home .']
+    fras = ['va !', 'j\'ai perdu .', 'il est calme .', 'je suis chez moi .']
+    for eng, fra in zip(engs, fras):
+        translation, attention_weight_seq = predict_seq2seq(
+            net, eng, src_vocab, tgt_vocab, num_steps, device)
+    print(f'{eng} => {translation}, bleu {bleu(translation, fra, k=2):.3f}')
