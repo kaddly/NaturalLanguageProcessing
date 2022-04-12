7aec60f675aee6af632aa806cff3cd3007fe3dd3
cxz <1838038611@qq.com>
2022-04-12 15:30:14 +0800

    update

--- Attention/utils.py
+++ Attention/utils.py
@@ -1,4 +1,6 @@
 import torch
+from torch import nn
+import math
 import matplotlib.pyplot as plt
 
 
@@ -18,5 +20,25 @@ def show_heatmaps(matrices, xlabel, ylabel, titles=None, figsize=(2.5, 2.5), cma
     plt.show()
 
 
-attention_weights = torch.eye(10).reshape((1, 1, 10, 10))
-show_heatmaps(attention_weights, xlabel='Keys', ylabel='Queries')
+def sequence_mask(X, valid_len, value=0):
+    """在序列中屏蔽不相关的项"""
+    maxlen = X.size(1)
+    mask = torch.arange((maxlen), dtype=torch.float32, device=X.device)[None, :] < valid_len[:, None]
+    X[~mask] = value
+    return X
+
+
+def masked_softmax(X, valid_lens):
+    """通过在最后⼀个轴上掩蔽元素来执⾏softmax操作"""
+    # X:3D张量，valid_lens:1D或2D张量
+    if valid_lens is None:
+        return nn.functional.softmax(X, dim=-1)
+    else:
+        shape = X.shape
+        if valid_lens.dim() == 1:
+            valid_lens = torch.repeat_interleave(valid_lens, shape[1])
+        else:
+            valid_lens = valid_lens.shape(-1)
+        # 最后⼀轴上被掩蔽的元素使⽤⼀个⾮常⼤的负值替换，从⽽其softmax输出为0
+        X = sequence_mask(X.reshape(-1, shape[-1]), valid_lens, value=-1e6)
+        return nn.functional.softmax(X.reshape(shape), dim=-1)
