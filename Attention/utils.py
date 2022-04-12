9baca50ac8eb8ea43073413aa033cde48e544f32
cxz <1838038611@qq.com>
2022-04-12 15:15:15 +0800

    update

--- Attention/utils.py
+++ Attention/utils.py
@@ -2,8 +2,21 @@ import torch
 import matplotlib.pyplot as plt
 
 
-def show_heatmaps(matrices, xlabel, ylabel, title=None, figsize=(2.5, 2.5), cmap='red'):
-    num_rows, num_cols = matrices[0], matrices[1]
+def show_heatmaps(matrices, xlabel, ylabel, titles=None, figsize=(2.5, 2.5), cmap='Reds'):
+    num_rows, num_cols = matrices.shape[0], matrices.shape[1]
     fig, axes = plt.subplots(num_rows, num_cols, figsize=figsize, sharex=True, sharey=True, squeeze=False)
     for i, (row_axes, row_matrices) in enumerate(zip(axes, matrices)):
-        pass
+        for j, (ax, matrice) in enumerate(zip(row_axes, row_matrices)):
+            pcm = ax.imshow(matrice.detach().numpy(), cmap=cmap)
+            if i == num_rows - 1:
+                ax.set_xlabel(xlabel)
+            if j == 0:
+                ax.set_ylabel(ylabel)
+            if titles:
+                ax.set_title(titles[j])
+    fig.colorbar(pcm, ax=axes, shrink=0.6)
+    plt.show()
+
+
+attention_weights = torch.eye(10).reshape((1, 1, 10, 10))
+show_heatmaps(attention_weights, xlabel='Keys', ylabel='Queries')
