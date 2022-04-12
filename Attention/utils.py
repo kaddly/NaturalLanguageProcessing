ecd842d91fd19bb97da383081603ad861019ad82
cxz <1838038611@qq.com>
2022-04-12 11:40:31 +0800

    update

--- Attention/utils.py
+++ Attention/utils.py
@@ -1,1 +1,9 @@
 import torch
+import matplotlib.pyplot as plt
+
+
+def show_heatmaps(matrices, xlabel, ylabel, title=None, figsize=(2.5, 2.5), cmap='red'):
+    num_rows, num_cols = matrices[0], matrices[1]
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=figsize, sharex=True, sharey=True, squeeze=False)
+    for i, (row_axes, row_matrices) in enumerate(zip(axes, matrices)):
+        pass
