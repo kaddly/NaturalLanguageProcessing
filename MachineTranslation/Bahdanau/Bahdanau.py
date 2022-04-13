1354ecbbf3191919ef2a6d1b4ed2c8ca0b82a1d1
cxz <1838038611@qq.com>
2022-04-13 09:11:21 +0800

    update

---
+++ MachineTranslation/Bahdanau/Bahdanau.py
@@ -0,0 +1,50 @@
+import torch
+
+import torch.nn as nn
+
+
+class Encoder(nn.Module):
+    """编码器-解码器架构的基本编码器接⼝"""
+
+    def __init__(self, **kwargs):
+        super(Encoder, self).__init__(**kwargs)
+
+    def forward(self, X, *args):
+        raise NotImplementedError
+
+
+class Decoder(nn.Module):
+    """编码器-解码器架构的基本解码器接⼝"""
+
+    def __init__(self, **kwargs):
+        super(Decoder, self).__init__(**kwargs)
+
+    def init_state(self, enc_outputs, *args):
+        raise NotImplementedError
+
+    def forward(self, X, state):
+        raise NotImplementedError
+
+
+class EncoderDecoder(nn.Module):
+    """编码器-解码器架构的基类"""
+
+    def __init__(self, encoder, decoder, **kwargs):
+        super(EncoderDecoder, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, enc_X, dec_X, *args):
+        enc_outputs = self.encoder(enc_X)
+        dec_state = self.decoder.init_state(enc_outputs, *args)
+        return self.decoder(dec_X, dec_state)
+
+
+class AttentionDecoder(Decoder):
+    """带有注意⼒机制解码器的基本接⼝"""
+    def __init__(self, **kwargs):
+        super(AttentionDecoder, self).__init__(**kwargs)
+
+    @property
+    def attention_weights(self):
+        raise NotImplementedError
