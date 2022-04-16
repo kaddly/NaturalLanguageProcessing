a2b8ff6e2af480ee193a4e30f6f1e70059c0bf1e
cxz <1838038611@qq.com>
2022-04-16 10:14:16 +0800

    update

--- MachineTranslation/Transformer/Transformer.py
+++ MachineTranslation/Transformer/Transformer.py
@@ -40,6 +40,17 @@ class EncoderDecoder(nn.Module):
         return self.decoder(dec_X, dec_state)
 
 
+class AttentionDecoder(Decoder):
+    """带有注意⼒机制解码器的基本接⼝"""
+
+    def __init__(self, **kwargs):
+        super(AttentionDecoder, self).__init__(**kwargs)
+
+    @property
+    def attention_weights(self):
+        raise NotImplementedError
+
+
 def sequence_mask(X, valid_len, value=0):
     """在序列中屏蔽不相关的项"""
     maxlen = X.size(1)
@@ -292,3 +303,37 @@ class DecoderBLock(nn.Module):
         Y2 = self.attention2(Y, enc_outputs, enc_outputs, enc_valid_lens)
         Z = self.addnorm2(Y, Y2)
         return self.addnorm3(Z, Y2), state
+
+
+class TransformerDecoder(AttentionDecoder):
+    def __init__(self, vocab_size, key_size, query_size, value_size, num_hiddens, norm_shape, ffn_num_input,
+                 ffn_num_hiddens, num_heads, num_layers, dropout, **kwargs):
+        super(TransformerDecoder, self).__init__(**kwargs)
+        self.num_hiddens = num_hiddens
+        self.num_layers = num_layers
+        self.embedding = nn.Embedding(vocab_size, num_hiddens)
+        self.pos_encoding = PositionalEncoding(num_hiddens, dropout)
+        self.blks = nn.Sequential()
+        for i in range(num_layers):
+            self.blks.add_module("block" + str(i),
+                                 DecoderBLock(key_size, query_size, value_size, num_hiddens, norm_shape, ffn_num_input,
+                                              ffn_num_hiddens, num_heads, dropout))
+        self.dense = nn.Linear(num_hiddens, vocab_size)
+
+    def init_state(self, enc_outputs, enc_valid_lens, *args):
+        return [enc_outputs, enc_valid_lens, [None] * self.num_layers]
+
+    def forward(self, X, state):
+        X = self.pos_encoding(self.embedding(X) * math.sqrt(self.num_hiddens))
+        self._attention_weights = [[None] * len(self.blks) for _ in range(2)]
+        for i, blk in enumerate(self.blks):
+            X, state = blk(X, state)
+            # 解码器⾃注意⼒权重
+            self._attention_weights[0][i] = blk.attention1.attention.attention_weights
+            # “编码器－解码器”⾃注意⼒权重
+            self._attention_weights[1][i] = blk.attention2.attention.attention_weights
+
+        return self.dense(X), state
+
+    def attention_weights(self):
+        return self._attention_weights
