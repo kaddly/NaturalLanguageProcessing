a0cff4a3db051cbbf09a94590ac5294f6fa65252
cxz <1838038611@qq.com>
2022-04-16 09:51:55 +0800

    update

--- MachineTranslation/Transformer/Transformer.py
+++ MachineTranslation/Transformer/Transformer.py
@@ -250,3 +250,45 @@ class TransformerEncoder(Encoder):
             X = blk(X, valid_len)
             self.attention_weights[i] = blk.attention.attention.attention_weights
         return X
+
+
+class DecoderBLock(nn.Module):
+    """解码器中第i个块"""
+
+    def __init__(self, key_size, query_size, values_size, num_hiddens, norm_shape, ffn_num_input, ffn_num_hiddens,
+                 num_heads, dropout, i, **kwargs):
+        super(DecoderBLock, self).__init__(**kwargs)
+        self.i = i
+        self.attention1 = MultiHeadAttention(key_size, query_size, values_size, num_hiddens, num_heads, dropout)
+        self.addnorm1 = AddNorm(norm_shape, dropout)
+        self.attention2 = MultiHeadAttention(key_size, query_size, values_size, num_hiddens, num_heads, dropout)
+        self.addnorm2 = AddNorm(norm_shape, dropout)
+        self.ffn = PositionWiseFFN(ffn_num_input, ffn_num_hiddens, num_hiddens)
+        self.addnorm3 = AddNorm(norm_shape, dropout)
+
+    def forward(self, X, state):
+        enc_outputs, enc_valid_lens = state[0], state[1]
+        # 训练阶段，输出序列的所有词元都在同⼀时间处理，
+        # 因此state[2][self.i]初始化为None。
+        # 预测阶段，输出序列是通过词元⼀个接着⼀个解码的，
+        # 因此state[2][self.i]包含着直到当前时间步第i个块解码的输出表⽰
+        if state[2][self.i] is None:
+            key_values = X
+        else:
+            key_values = torch.cat((state[2][self.i], X), axis=1)
+        state[2][self.i] = key_values
+        if self.training:
+            batch_size, num_steps, _ = X.shape
+            # dec_valid_lens的开头:(batch_size,num_steps),
+            # 其中每⼀⾏是[1,2,...,num_steps]
+            dec_valid_lens = torch.arange(1, num_steps + 1, device=X.device).repeat(batch_size, 1)
+        else:
+            dec_valid_lens = None
+        # 自注意力
+        X2 = self.attention1(X, key_values, key_values, dec_valid_lens)
+        Y = self.addnorm1(X, X2)
+        # 编码器－解码器注意⼒。
+        # enc_outputs的开头:(batch_size,num_steps,num_hiddens)
+        Y2 = self.attention2(Y, enc_outputs, enc_outputs, enc_valid_lens)
+        Z = self.addnorm2(Y, Y2)
+        return self.addnorm3(Z, Y2), state
