bd596326591d4b3841d6e28390612854043eb25b
cxz <1838038611@qq.com>
2022-04-18 16:58:57 +0800

    update

--- PreTrain/Word2vec/train_eval.py
+++ PreTrain/Word2vec/train_eval.py
@@ -5,7 +5,7 @@ import time
 
 class SigmoidBCELoss(nn.Module):
     def __init__(self):
-        super.__init__()
+        super(SigmoidBCELoss, self).__init__()
 
     def forward(self, inputs, target, mask=None):
         out = nn.functional.binary_cross_entropy_with_logits(inputs, target, weight=mask, reduction="none")
