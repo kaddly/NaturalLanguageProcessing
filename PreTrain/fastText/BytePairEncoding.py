58f1dea3da2a93bfd09b74647e25fa8658d5d8cc
cxz <1838038611@qq.com>
2022-04-20 11:03:11 +0800

    update

---
+++ PreTrain/fastText/BytePairEncoding.py
@@ -0,0 +1,38 @@
+import collections
+
+symbols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+           'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+           '_', '[UNK]']
+raw_token_freqs = {'fast_': 4, 'faster_': 3, 'tall_': 5, 'taller_': 4}
+token_freqs = {}
+for token, freq in raw_token_freqs.items():
+    token_freqs[' '.join(list(token))] = raw_token_freqs[token]
+print(token_freqs)
+
+
+def get_max_freq_pair(token_freqs):
+    pairs = collections.defaultdict(int)
+    for token, freq in token_freqs.items():
+        symbols = token.split()
+        for i in range(len(symbols) - 1):
+            # “pairs”的键是两个连续符号的元组
+            pairs[symbols[i], symbols[i + 1]] += freq
+    return max(pairs, key=pairs.get)  # 具有最⼤值的“pairs”键
+
+
+def merge_symbols(max_freq_pair, token_freqs, symbols):
+    symbols.append(''.join(max_freq_pair))
+    new_token_freqs = dict()
+    for token, frep in token_freqs.items():
+        new_token = token.replace(' '.join(max_freq_pair), ''.join(max_freq_pair))
+        new_token_freqs[new_token] = token_freqs[token]
+    return new_token_freqs
+
+
+num_merges = 10
+for i in range(num_merges):
+    max_freq_pair = get_max_freq_pair(token_freqs)
+    token_freqs = merge_symbols(max_freq_pair, token_freqs, symbols)
+    print(f'合并# {i + 1}:', max_freq_pair)
+
+print(symbols)
