a16970de9098f068a8c10a720bf0ac3d4b61dc2e
cxz <1838038611@qq.com>
2022-04-22 10:58:24 +0800

    update

--- PreTrain/Bert/Bert.py
+++ PreTrain/Bert/Bert.py
@@ -169,17 +169,6 @@ class EncoderBlock(nn.Module):
         return self.addnorm2(Y, self.ffn(Y))
 
 
-def get_tokens_and_segments(tokens_a, tokens_b=None):
-    """获取输⼊序列的词元及其⽚段索引"""
-    tokens = ['<cls>'] + tokens_a + ['<sep>']
-    # 0和1分别标记⽚段A和B
-    segment = [0] * (len(tokens_a) + 2)
-    if tokens_b is not None:
-        tokens += tokens_b + ['<sep>']
-        segment += [1] * (len(tokens_b) + 1)
-    return tokens, segment
-
-
 class BERTEncoder(nn.Module):
     """BERT编码器"""
 
