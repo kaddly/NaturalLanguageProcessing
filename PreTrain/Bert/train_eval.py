5e1415ad2d52df3cde0c2e20af58caf92e085943
cxz <1838038611@qq.com>
2022-04-24 14:41:18 +0800

    update

--- PreTrain/Bert/train_eval.py
+++ PreTrain/Bert/train_eval.py
@@ -1,7 +1,49 @@
 import torch
+from torch import nn
+import time
 
 
 def _get_batch_loss_bert(net, loss, vocab_size, tokens_X, segments_X, valid_lens_x, pred_positions_X, mlm_weights_X,
                          mlm_Y, nsp_y):
     # 前向传播
     _, mlm_Y_hat, nsp_Y_hat = net(tokens_X, segments_X, valid_lens_x.reshape(-1), pred_positions_X)
+    # 计算遮蔽语⾔模型损失
+    mlm_l = loss(mlm_Y_hat.reshape(-1, vocab_size), mlm_Y.reshape(-1)) * mlm_weights_X.reshape(-1, 1)
+    mlm_l = mlm_l.sum() / (mlm_weights_X.sum() + 1e-8)
+    # 计算下⼀句⼦预测任务的损失
+    nsp_l = loss(nsp_Y_hat, nsp_y)
+    l = mlm_l + nsp_l
+    return mlm_l, nsp_l,
+
+
+def train_bert(train_iter, net, loss, vocab_size, devices, num_steps):
+    net = nn.DataParallel(net, device_ids=devices).to(devices[0])
+    trainer = torch.optim.Adam(net.parameters(), lr=0.01)
+    step = 0
+    num_steps_reached = False
+    mlm_ls, nsp_ls = [], []
+    with step < num_steps and not num_steps_reached:
+        for tokens_X, segments_X, valid_lens_x, pred_positions_X, mlm_weights_X, mlm_Y, nsp_y in train_iter:
+            tokens_X = tokens_X.to(devices[0])
+            segments_X = segments_X.to(devices[0])
+            valid_lens_x = valid_lens_x.to(devices[0])
+            pred_positions_X = pred_positions_X.to(devices[0])
+            mlm_weights_X = mlm_weights_X.to(devices[0])
+            mlm_Y, nsp_y = mlm_Y.to(devices[0]), nsp_y.to(devices[0])
+            trainer.zero_grad()
+            tik = time.time()
+            mlm_l, nsp_l, l = _get_batch_loss_bert(net, loss, vocab_size, tokens_X, segments_X, valid_lens_x,
+                                                   pred_positions_X, mlm_weights_X, mlm_Y, nsp_y)
+            l.backward()
+            trainer.step()
+            mlm_ls.append(mlm_l)
+            nsp_ls.append(nsp_l)
+            print(f"step:{step + 1}>>MLM_loss:{sum(mlm_ls)/len(mlm_ls):.3f}>>nsp_loss:{sum(nsp_ls)/len(nsp_ls):.3f}")
+            step += 1
+            if step == num_steps:
+                num_steps_reached = True
+                break
+    print(f'MLM loss {sum(mlm_ls)/len(mlm_ls):.3f}, '
+          f'NSP loss {sum(nsp_ls)/len(nsp_ls):.3f}')
+    print(f'{tokens_X.shape[0]*num_steps/(time.time() - tik):.1f} sentence pairs/sec on '
+          f'{str(devices)}')
