3f910513dbbda2831cbc1445f292786c7a8f998d
cxz <1838038611@qq.com>
2022-04-26 10:11:57 +0800

    update

--- SentimentAnalysis/BiRNN/utils.py
+++ SentimentAnalysis/BiRNN/utils.py
@@ -1,6 +1,79 @@
 import torch
-from torch import nn
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader
 import os
+import collections
+import matplotlib.pyplot as plt
+
+
+def tokenize(lines, token='word'):
+    if token == 'word':
+        return [line.split() for line in lines]
+    elif token == 'char':
+        return [list(line) for line in lines]
+    else:
+        print("未知类型：" + token)
+
+
+def truncate_pad(line, num_steps, padding_token):
+    """Truncate or pad sequences.
+
+    Defined in :numref:`sec_machine_translation`"""
+    if len(line) > num_steps:
+        return line[:num_steps]  # Truncate
+    return line + [padding_token] * (num_steps - len(line))  # Pad
+
+
+# 词表
+class Vocab:
+    def __init__(self, tokens=None, min_freq=0, reserved_tokens=None):
+        if tokens is None:
+            tokens = []
+        if reserved_tokens is None:
+            reserved_tokens = []
+        # 按照频率统计出现的次数
+        counter = count_corpus(tokens)
+        self._token_freqs = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+
+        # 未知词元索引为0
+        self.idx_to_token = ['<UNK>'] + reserved_tokens
+        self.token_to_idx = {token: idx for idx, token in enumerate(self.idx_to_token)}
+        # self.idx_to_token, self.token_to_idx = [], dict()
+        for token, freq in self._token_freqs:
+            if freq < min_freq:
+                break
+            if token not in self.token_to_idx:
+                self.idx_to_token.append(token)
+                self.token_to_idx[token] = len(self.idx_to_token) - 1
+
+    def __len__(self):
+        return len(self.idx_to_token)
+
+    def __getitem__(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    def to_tokens(self, indices):
+        if not isinstance(indices, (list, tuple)):
+            return self.idx_to_token[indices]
+        return [self.idx_to_token[index] for index in indices]
+
+    @property
+    def unk(self):  # 未知词元的索引为0
+        return 0
+
+    @property
+    def token_freqs(self):
+        return self._token_freqs
+
+
+def count_corpus(tokens):
+    """统计词元的频率"""
+    # 这里的tokens是1d或者2d列表
+    if len(tokens) == 0 or isinstance(tokens[0], list):
+        tokens = [token for line in tokens for token in line]
+    return collections.Counter(tokens)
 
 
 def read_imdb(data_dir, is_train):
@@ -16,8 +89,30 @@ def read_imdb(data_dir, is_train):
     return data, labels
 
 
-data_dir = '../data/aclImdb'
-train_data = read_imdb(data_dir, is_train=True)
-print('训练集数⽬：', len(train_data[0]))
-for x, y in zip(train_data[0][:3], train_data[1][:3]):
-    print('标签：', y, 'review:', x[0:60])
+# data_dir = '../data/aclImdb'
+# train_data = read_imdb(data_dir, is_train=True)
+# print('训练集数⽬：', len(train_data[0]))
+# for x, y in zip(train_data[0][:3], train_data[1][:3]):
+#     print('标签：', y, 'review:', x[0:60])
+# train_tokens = tokenize(train_data[0], token='word')
+# vocab = Vocab(train_tokens, min_freq=5, reserved_tokens=['<pad>'])
+# plt.xlabel('# tokens per review')
+# plt.ylabel('count')
+# plt.hist([len(line) for line in train_tokens], bins=range(0, 1000, 50))
+# plt.show()
+
+def load_data_imdb(batch_size, num_steps=500):
+    """返回数据迭代器和IMDB评论数据集的词表"""
+    data_dir = '../data/aclImdb'
+    train_data = read_imdb(data_dir, is_train=True)
+    test_data = read_imdb(data_dir, False)
+    train_tokens = tokenize(train_data[0], token='word')
+    test_tokens = tokenize(test_data[0], token='word')
+    vocab = Vocab(train_tokens, min_freq=5)
+    train_features = torch.tensor([truncate_pad(vocab[line], num_steps, vocab['<pad>']) for line in train_tokens])
+    test_features = torch.tensor([truncate_pad(vocab[line], num_steps, vocab['<pad>']) for line in test_tokens])
+    train_dataset = TensorDataset(*(train_features, torch.tensor(train_data[1])))
+    train_iter = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_dataset = TensorDataset(*(test_features, torch.tensor(test_data[1])))
+    test_iter = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    return train_iter, test_iter, vocab
