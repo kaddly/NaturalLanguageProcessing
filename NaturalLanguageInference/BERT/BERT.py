1c33fe749e09f73cce45e1004cf619c43ed7a236
cxz <1838038611@qq.com>
2022-04-30 09:41:53 +0800

    update

---
+++ NaturalLanguageInference/BERT/BERT.py
@@ -0,0 +1,7 @@
+import json
+import multiprocessing
+import os
+import torch
+from torch import nn
+
+
---
+++ NaturalLanguageInference/BERT/data_utils.py
@@ -0,0 +1,65 @@
+import os
+import re
+import torch
+from torch.utils.data import Dataset, DataLoader
+from token_utils import Vocab, tokenize, truncate_pad
+
+
+def read_snli(data_dir, is_train):
+    """将SNLI数据集解析为前提、假设和标签"""
+
+    def extract_text(s):
+        # 删除我们不会使⽤的信息
+        s = re.sub('\\(', '', s)
+        s = re.sub('\\)', '', s)
+        # ⽤⼀个空格替换两个或多个连续的空格
+        s = re.sub('\\s{2,}', ' ', s)
+        return s.strip()
+
+    label_set = {'entailment': 0, 'contradiction': 1, 'neutral': 2}
+    file_name = os.path.join(data_dir, 'snli_1.0_train.txt' if is_train else 'snli_1.0_test.txt')
+    with open(file_name, 'r') as f:
+        rows = [row.split('\t') for row in f.readlines()[1:]]
+    premises = [extract_text(row[1]) for row in rows if row[0] in label_set]
+    hypotheses = [extract_text(row[2]) for row in rows if row[0] in label_set]
+    labels = [label_set[row[0]] for row in rows if row[0] in label_set]
+    return premises, hypotheses, labels
+
+
+class SNLIDataset(Dataset):
+    """⽤于加载SNLI数据集的⾃定义数据集"""
+
+    def __init__(self, dataset, num_steps, vocab=None):
+        self.num_steps = num_steps
+        all_premise_tokens = tokenize(dataset[0])
+        all_hypothesis_tokens = tokenize(dataset[1])
+        if vocab is None:
+            self.vocab = Vocab(all_premise_tokens + all_hypothesis_tokens, min_freq=5, reserved_tokens=['<pad>'])
+        else:
+            self.vocab = vocab
+        self.premises = self._pad(all_premise_tokens)
+        self.hypotheses = self._pad(all_hypothesis_tokens)
+        self.labels = torch.tensor(dataset[2])
+        print('read ' + str(len(self.premises)) + ' examples')
+
+    def _pad(self, lines):
+        return torch.tensor([truncate_pad(self.vocab[line], self.num_steps, self.vocab['<pad>']) for line in lines])
+
+    def __getitem__(self, idx):
+        return (self.premises[idx], self.hypotheses[idx]), self.labels[idx]
+
+    def __len__(self):
+        return len(self.premises)
+
+
+def load_data_snli(batch_size, num_steps=50):
+    """下载SNLI数据集并返回数据迭代器和词表"""
+    # num_workers = 4
+    data_dir = '../data/snli_1.0/'
+    train_data = read_snli(data_dir, True)
+    test_data = read_snli(data_dir, False)
+    train_set = SNLIDataset(train_data, num_steps)
+    train_iter = DataLoader(train_set, batch_size, shuffle=True)
+    test_set = SNLIDataset(test_data, num_steps, train_set.vocab)
+    test_iter = DataLoader(test_set, batch_size, shuffle=False)
+    return train_iter, test_iter, train_set.vocab
