d17462ff377a5a15b58577926b41753731908104
cxz <1838038611@qq.com>
2022-05-04 15:15:03 +0800

    update

--- Text-Classification/BiRNN/train_eval.py
+++ Text-Classification/BiRNN/train_eval.py
@@ -1,6 +1,50 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 import time
+from datetime import timedelta
+from sklearn import metrics
+import numpy as np
+
+save_path = './saved_dict/'  # 模型训练结果
+class_list = [x.strip() for x in open('../data/THUCNews/data/class.txt', encoding='utf-8').readlines()]  # 类别名单
+
+
+def accuracy(y_hat, y):
+    """Compute the number of correct predictions.
+
+    Defined in :numref:`sec_softmax_scratch`"""
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = torch.argmax(y_hat, axis=1)
+    cmp = y_hat.to(dtype=y.dtype) == y
+    cmp = cmp.to(dtype=y.dtype)
+    return float(cmp.sum()) / len(cmp)
+
+
+def evaluate_accuracy_gpu(net, data_iter, device=None):
+    """Compute the accuracy for a model on a dataset using a GPU.
+
+    Defined in :numref:`sec_lenet`"""
+    if isinstance(net, nn.Module):
+        net.eval()  # Set the model to evaluation mode
+        if not device:
+            device = next(iter(net.parameters())).device
+    # No. of correct predictions, no. of predictions
+
+    with torch.no_grad():
+        acc, nums, loss = 0, 0, 0
+        for X, y in data_iter:
+            if isinstance(X, list):
+                # Required for BERT Fine-tuning (to be covered later)
+                X = [x.to(device) for x in X]
+            else:
+                X = X.to(device)
+            y = y.to(device)
+            y_hat = net(X)
+            acc += accuracy(y_hat, y)
+            loss += F.cross_entropy(y_hat, y)
+            nums += len(y)
+    return acc / nums, loss / nums
 
 
 def train(model, train_iter, dev_iter, test_iter, loss, devices, lr, num_epochs):
@@ -36,5 +80,65 @@ def train(model, train_iter, dev_iter, test_iter, loss, devices, lr, num_epochs)
             optimizer.step()
             if total_batch % 50 == 0:
                 # 每多少轮输出在训练集和验证集上的效果
-                true = labels.data.cpu()
-                predic = torch.max(outputs.data, 1)[1].cpu()
+                train_acc = accuracy(outputs, labels)
+                dev_acc, dev_loss = evaluate_accuracy_gpu(model, dev_iter)
+                if dev_loss < dev_best_loss:
+                    dev_best_loss = dev_loss
+                    torch.save(model.state_dict(), save_path + 'BiRNN.ckpt')
+                    improve = '*'
+                    last_improve = total_batch
+                else:
+                    improve = ''
+                time_dif = timedelta(seconds=int(round(time.time() - start_time)))
+                msg = 'Iter: {0:>6},  Train Loss: {1:>5.2},  Train Acc: {2:>6.2%},  Val Loss: {3:>5.2},  Val Acc: {4:>6.2%},  Time: {5} {6}'
+                print(msg.format(total_batch, loss.item(), train_acc, dev_loss, dev_acc, time_dif, improve))
+
+                model.train()
+            total_batch += 1
+            if total_batch - last_improve > 1000:
+                # 验证集loss超过1000batch没下降，结束训练
+                print("No optimization for a long time, auto-stopping...")
+                flag = True
+                break
+        if flag:
+            break
+    test(model, test_iter)
+
+
+def test(model, test_iter):
+    # test
+    model.load_state_dict(torch.load(save_path + 'BiRNN.ckpt'))
+    model.eval()
+    start_time = time.time()
+    test_acc, test_loss, test_report, test_confusion = evaluate(model, test_iter, test=True)
+    msg = 'Test Loss: {0:>5.2},  Test Acc: {1:>6.2%}'
+    print(msg.format(test_loss, test_acc))
+    print("Precision, Recall and F1-Score...")
+    print(test_report)
+    print("Confusion Matrix...")
+    print(test_confusion)
+    time_dif = timedelta(seconds=int(round(time.time() - start_time)))
+    print("Time usage:", time_dif)
+
+
+def evaluate(model, data_iter, test=False):
+    model.eval()
+    loss_total = 0
+    predict_all = np.array([], dtype=int)
+    labels_all = np.array([], dtype=int)
+    with torch.no_grad():
+        for texts, labels in data_iter:
+            outputs = model(texts)
+            loss = F.cross_entropy(outputs, labels)
+            loss_total += loss
+            labels = labels.data.cpu().numpy()
+            predic = torch.max(outputs.data, 1)[1].cpu().numpy()
+            labels_all = np.append(labels_all, labels)
+            predict_all = np.append(predict_all, predic)
+
+    acc = metrics.accuracy_score(labels_all, predict_all)
+    if test:
+        report = metrics.classification_report(labels_all, predict_all, target_names=class_list, digits=4)
+        confusion = metrics.confusion_matrix(labels_all, predict_all)
+        return acc, loss_total / len(data_iter), report, confusion
+    return acc, loss_total / len(data_iter)
