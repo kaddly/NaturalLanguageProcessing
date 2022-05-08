61ffc7e9505d4841238987e20fc09d4c826f5181
cxz <1838038611@qq.com>
2022-05-08 11:18:47 +0800

    update

--- PreTrain/GPT/data_utils.py
+++ PreTrain/GPT/data_utils.py
@@ -14,9 +14,26 @@ def _text_standardize(text):
 
 def read_MPRCData(data_dir, is_train=True):
     is_next_labels, contents = [], []
-    with open(os.path.join(data_dir, 'msr_paraphrase_train.txt' if is_train else 'msr_paraphrase_test.txt'), encoding='UTF-8') as f:
+    with open(os.path.join(data_dir, 'msr_paraphrase_train.txt' if is_train else 'msr_paraphrase_test.txt'),
+              encoding='UTF-8') as f:
         for line in f.readlines()[1:]:
             line = line.split('\t')
             is_next_labels.append(line[0])
             contents.append((_text_standardize(line[-2]), _text_standardize(line[-1])))
     return contents, is_next_labels
+
+
+# ⽣成下⼀句预测任务的数据
+def get_tokens_and_segments(tokens_a, tokens_b=None):
+    """获取输⼊序列的词元及其⽚段索引"""
+    tokens = ['<cls>'] + tokens_a + ['<sep>']
+    # 0和1分别标记⽚段A和B
+    segment = [0] * (len(tokens_a) + 2)
+    if tokens_b is not None:
+        tokens += tokens_b + ['<sep>']
+        segment += [1] * (len(tokens_b) + 1)
+    return tokens, segment
+
+
+def _pad_GPT_input(examples, max_len, vocab):
+    pass
