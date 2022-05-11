f4944a108682a83bc2767f197eb95f0121f357ce
cxz <1838038611@qq.com>
2022-05-11 10:00:07 +0800

    update

--- PreTrain/GPT/train_eval.py
+++ PreTrain/GPT/train_eval.py
@@ -1,3 +1,34 @@
 import torch
 from torch import nn
+from GPT import sequence_mask
 
+
+class MaskedSoftmaxCELoss(nn.CrossEntropyLoss):
+    """带遮蔽的softmax交叉熵损失函数"""
+
+    # `pred` 的形状：(`batch_size`, `num_steps`, `vocab_size`)
+    # `label` 的形状：(`batch_size`, `num_steps`)
+    # `valid_len` 的形状：(`batch_size`,)
+    def forward(self, pred, label, valid_len):
+        weights = torch.ones_like(label)
+        weights = sequence_mask(weights, valid_len)
+        self.reduction = 'none'
+        unweighted_loss = super(MaskedSoftmaxCELoss, self).forward(pred.permute(0, 2, 1), label)
+        weighted_loss = (unweighted_loss * weights).mean(dim=1)
+        return weighted_loss
+
+
+def grad_clipping(net, theta):  # @save
+    """裁剪梯度"""
+    if isinstance(net, nn.Module):
+        params = [p for p in net.parameters() if p.requires_grad]
+    else:
+        params = net.params
+    norm = torch.sqrt(sum(torch.sum((p.grad ** 2)) for p in params))
+    if norm > theta:
+        for param in params:
+            param.grad[:] *= theta / norm
+
+
+def train_GPT(train_iter, test_iter):
+    pass
