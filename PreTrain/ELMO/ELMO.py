2952b184beec7ade295462590b737eb58e3ea2d3
cxz <1838038611@qq.com>
2022-05-16 14:49:27 +0800

    update

--- PreTrain/ELMO/ELMO.py
+++ PreTrain/ELMO/ELMO.py
@@ -2,3 +2,9 @@ import torch
 from torch import nn
 
 
+class ELMO(nn.Module):
+    def __init__(self, vocab_size, run_layer, **kwargs):
+        super(ELMO, self).__init__(**kwargs)
+        self.rnn = run_layer
+        self.num_hiddens = self.rnn.hidden_size
+        self.embedding = nn.Embedding(vocab_size, self.num_hiddens)
