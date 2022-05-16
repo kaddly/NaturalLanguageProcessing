eb4aba874a9247885d63f3cf544ec3014727b534
cxz <1838038611@qq.com>
2022-05-16 11:12:31 +0800

    update

--- PreTrain/ELMO/data_utils.py
+++ PreTrain/ELMO/data_utils.py
@@ -2,7 +2,7 @@ import os
 import random
 import torch
 from torch.utils.data import DataLoader, Dataset
-from token_utils import Vocab, tokenize, truncate_pad
+from token_utils import Vocab, tokenize
 
 
 def _read_wiki(data_dir):
@@ -16,6 +16,25 @@ def _read_wiki(data_dir):
     return paragraphs
 
 
+def _seq_data_cut(corpus, num_steps):
+    corpus = corpus[random.randint(0, num_steps - 1):]
+    num_subseqs = (len(corpus) - 1) // num_steps
+    initial_indices = list(range(0, num_subseqs * num_steps, num_steps))
+
+    def data(pos):
+        # 返回从pos位置开始的长度为num_steps的子序列
+        return corpus[pos:num_steps + pos]
+
+    seqs = []
+    seqs_fw = []
+    seqs_bw = []
+    for pos in initial_indices:
+        seqs.append(data(pos))
+        seqs_fw.append(data(pos+1))
+        seqs_bw.append(data(pos-1))
+    return seqs, seqs_fw, seqs_bw
+
+
 class _WikiTextDataset(Dataset):
     def __init__(self, paragraphs, num_steps):
         paragraphs = [tokenize(paragraph, token='word') for paragraph in paragraphs]
@@ -23,17 +42,10 @@ class _WikiTextDataset(Dataset):
         self.vocab = Vocab(sentences, min_freq=5)
         self.num_steps = num_steps
         corpus = [self.vocab[token] for line in sentences for token in line]
-        self.corpus = corpus[random.randint(0, self.num_steps - 1):]
-        num_subseqs = (len(self.corpus) - 1) // self.num_steps
-        self.initial_indices = list(range(0, num_subseqs * self.num_steps, self.num_steps))
-        random.shuffle(self.initial_indices)
+        self.seqs, self.seq_fw, self.seq_bw= _seq_data_cut(corpus, self.num_steps)
 
     def __getitem__(self, item):
-        pass
+        return self.seqs[item], self.seq_fw[item], self.seq_bw[item]
 
     def __len__(self):
-        pass
-
-    def data(self, pos):
-        # 返回从pos位置开始的长度为num_steps的子序列
-        return self.corpus[pos:self.num_steps + pos]
+        return len(self.seqs)
