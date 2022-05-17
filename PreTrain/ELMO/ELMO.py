4f1bb6f8c9c47afa220e4c376fe021701d7809ef
cxz <1838038611@qq.com>
2022-05-17 10:16:37 +0800

    update

--- PreTrain/ELMO/ELMO.py
+++ PreTrain/ELMO/ELMO.py
@@ -3,8 +3,52 @@ from torch import nn
 
 
 class ELMO(nn.Module):
-    def __init__(self, vocab_size, run_layer, **kwargs):
+    def __init__(self, vocab_size, embedding_size, hidden_size, num_layers, **kwargs):
         super(ELMO, self).__init__(**kwargs)
-        self.rnn = run_layer
-        self.num_hiddens = self.rnn.hidden_size
-        self.embedding = nn.Embedding(vocab_size, self.num_hiddens)
+        self.embedding = nn.Embedding(vocab_size, embedding_size)
+        # forward LSTM
+        self.fs = nn.ModuleList([nn.LSTM(input_size=embedding_size, hidden_size=hidden_size, bidirectional=False)
+                                 if i == 0 else
+                                 nn.LSTM(input_size=hidden_size, hidden_size=hidden_size, bidirectional=False)
+                                 for i in range(num_layers)])
+        self.f_dense = nn.Linear(hidden_size, vocab_size)
+        # backward LSTM
+        self.bs = nn.ModuleList([nn.LSTM(input_size=embedding_size, hidden_size=hidden_size, bidirectional=False)
+                                 if i == 0 else
+                                 nn.LSTM(input_size=hidden_size, hidden_size=hidden_size, bidirectional=False)
+                                 for i in range(num_layers)])
+        self.b_dense = nn.Linear(hidden_size, vocab_size)
+
+    def forward(self, seqs, state):
+        # (num_steps,batch_size,embedding_size)
+        embedding = self.embedding(seqs.T)
+        self.fxs = [embedding.permute(1, 0, 3)]
+        self.bxs = [embedding.permute(1, 0, 3)]
+        for fl, bl in zip(self.fs, self.bs):
+            output_f, state = fl(embedding, state)
+            self.fxs.append(output_f.permute(1, 0, 3))
+            output_b, state = bl(torch.flip(embedding, dims=[0, ]), state)
+            self.bxs.append(torch.flip(output_b, dims=[0, ]).permute(1, 0, 3))
+        return self.f_dense(self.fxs[-1]), self.b_dense(self.bxs[-1])
+
+    def begin_state(self, device, batch_size=1):
+        if not isinstance(self.fs[0], nn.LSTM):
+            # nn.GRU以张量作为隐状态
+            return torch.zeros((1, batch_size, self.hidden_size), device=device)
+        else:
+            # nn.LSTM以元组作为隐状态
+            return (
+                torch.zeros((1, batch_size, self.hidden_size), device=device),
+                torch.zeros((1, batch_size, self.hidden_size), device=device))
+
+    @property
+    def get_embedding(self):
+        xs = [
+                 torch.cat((self.fxs[0], self.bxs[0]), dim=2).cpu().data.numpy()
+             ] + [
+                 torch.cat((f, b), dim=2).cpu().data.numpy()
+                 for f, b in zip(self.fxs[1:], self.bxs[1:])
+             ]
+        for x in xs:
+            print("layers shape=", x.shape)
+        return xs
