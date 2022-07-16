5c0781d84135917fcd89207fedd81820c0ac3c6f
cxz <1838038611@qq.com>
2022-07-16 08:17:20 +0800

    update

--- PreTrain/RoBERTa/data_utils.py
+++ PreTrain/RoBERTa/data_utils.py
@@ -1,6 +1,16 @@
 import os
+import random
 import torch
 from torch.utils.data import Dataset, DataLoader
 
 
+def _read_wiki(data_dir):
+    file_name = os.path.join(data_dir, 'wiki.train.tokens')
+    with open(file_name, 'r', encoding='UTF-8') as f:
+        lines = f.readlines()
+    # ⼤写字⺟转换为⼩写字⺟
+    paragraphs = [line.strip().lower().split(' . ')
+                  for line in lines if len(line.split(' . ')) >= 2]
+    random.shuffle(paragraphs)
+    return paragraphs
 
