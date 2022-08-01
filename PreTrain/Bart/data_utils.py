e3101b4836c79f9d114855a3ff8f75648b366e30
cxz <1838038611@qq.com>
2022-08-01 09:24:10 +0800

    update

--- PreTrain/Bart/data_utils.py
+++ PreTrain/Bart/data_utils.py
@@ -1,6 +1,8 @@
+import os
 import torch
 from torch.utils.data import Dataset, DataLoader
 
 
-def read_data(data_dir):
-    pass
+def read_data(data_dir, file_name):
+    with open(os.path.join(data_dir, file_name), 'rb', encoding='UTF-8') as f:
+        f.readlines()
