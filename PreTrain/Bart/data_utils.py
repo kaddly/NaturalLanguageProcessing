1573a5bb31ff3fb57417f4e157ac1f25bd131df2
cxz <1838038611@qq.com>
2022-08-02 09:19:43 +0800

    update

--- PreTrain/Bart/data_utils.py
+++ PreTrain/Bart/data_utils.py
@@ -1,8 +1,31 @@
 import os
+from matplotlib.pyplot import plot as plt
+import random
 import torch
 from torch.utils.data import Dataset, DataLoader
 
 
-def read_data(data_dir, file_name):
-    with open(os.path.join(data_dir, file_name), 'rb', encoding='UTF-8') as f:
-        f.readlines()
+def show_heatmaps(matrices, xlabel, ylabel, titles=None, figsize=(2.5, 2.5), cmap='Reds'):
+    num_rows, num_cols = matrices.shape[0], matrices.shape[1]
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=figsize, sharex=True, sharey=True, squeeze=False)
+    for i, (row_axes, row_matrices) in enumerate(zip(axes, matrices)):
+        for j, (ax, matrice) in enumerate(zip(row_axes, row_matrices)):
+            pcm = ax.imshow(matrice.detach().numpy(), cmap=cmap)
+            if i == num_rows - 1:
+                ax.set_xlabel(xlabel)
+            if j == 0:
+                ax.set_ylabel(ylabel)
+            if titles:
+                ax.set_title(titles[j])
+    fig.colorbar(pcm, ax=axes, shrink=0.6)
+    plt.show()
+
+
+def _read_wiki(data_dir, file_name):
+    file_path = os.path.join(data_dir, file_name)
+    with open(file_path, 'r', encoding='UTF-8') as f:
+        lines = f.readlines()
+    # ⼤写字⺟转换为⼩写字⺟
+    paragraphs = [line.strip().lower().split(' . ') for line in lines if len(line.split(' . ')) >= 2]
+    random.shuffle(paragraphs)
+    return paragraphs
