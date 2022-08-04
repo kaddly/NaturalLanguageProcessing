0f5db1a688b200068040e1dfea01725621b36865
cxz <1838038611@qq.com>
2022-08-04 09:30:26 +0800

    update

--- PreTrain/Bart/utils/data_utils.py
+++ PreTrain/Bart/utils/data_utils.py
@@ -1,6 +1,8 @@
 import os
 import matplotlib.pyplot as plt
 import random
+import pickle
+from joblib import Parallel, delayed
 import torch
 from torch.utils.data import Dataset, DataLoader
 from utils.token_utils import BytePairEncoding
@@ -25,9 +27,52 @@ def show_heatmaps(matrices, xlabel, ylabel, titles=None, figsize=(2.5, 2.5), cma
 
 def _read_wiki(data_dir, file_name):
     file_path = os.path.join(data_dir, file_name)
+    contexts = []
     with open(file_path, 'r', encoding='UTF-8') as f:
-        lines = f.readlines()
-    # ⼤写字⺟转换为⼩写字⺟
-    paragraphs = [line.strip().lower().split(' . ') for line in lines if len(line.split(' . ')) >= 2]
-    random.shuffle(paragraphs)
-    return paragraphs
+        for line in f.readlines():
+            if len(line.split(' . ')) < 2:
+                continue
+            contexts.extend(line.strip().lower().split(' . '))
+    return contexts
+
+
+def BPE_Encoding(train_sentences, val_sentences, test_sentences, num_merge):
+    BPE = BytePairEncoding(train_sentences, num_merge, ['<unk>', '</w>', '<masked>', '<sep>'], min_freq=5)
+    if not os.path.exists(f'./data/BPE_Decoding_token{num_merge}/'):
+        train_tokens, val_tokens, test_tokens = Parallel(n_jobs=3)(
+            delayed(BPE.segment_BPE)(sentences) for sentences in [train_sentences, val_sentences, test_sentences])
+        os.mkdir(f'./data/BPE_Decoding_token{num_merge}/')
+        with open(f'./data/BPE_Decoding_token{num_merge}/train_tokens.plk', 'wb') as f:
+            pickle.dump(train_tokens, f)
+        with open(f'./data/BPE_Decoding_token{num_merge}/val_tokens.plk', 'wb') as f:
+            pickle.dump(val_tokens, f)
+        with open(f'./data/BPE_Decoding_token{num_merge}/test_tokens.plk', 'wb') as f:
+            pickle.dump(test_tokens, f)
+    else:
+        with open(f'./data/BPE_Decoding_token{num_merge}/train_tokens.plk', 'rb') as f:
+            train_tokens = pickle.load(f)
+        with open(f'./data/BPE_Decoding_token{num_merge}/val_tokens.plk', 'rb') as f:
+            val_tokens = pickle.load(f)
+        with open(f'./data/BPE_Decoding_token{num_merge}/test_tokens.plk', 'rb') as f:
+            test_tokens = pickle.load(f)
+    return train_tokens, val_tokens, test_tokens, BPE
+
+
+# ⽣成下⼀句预测任务的数据
+def get_tokens_and_segments(tokens_a, tokens_b=None):
+    """获取输⼊序列的词元及其⽚段索引"""
+    tokens = ['<bos>'] + tokens_a + ['<sep>']
+    if tokens_b is not None:
+        tokens += tokens_b + ['<eos>']
+    return tokens
+
+
+def load_data_wiki(batch_size, max_len, reconstruct_ways, num_merge=10000):
+    data_dir = './data/wikitext-2'
+    train_sentences = _read_wiki(data_dir, 'wiki.train.tokens')
+    val_sentences = _read_wiki(data_dir, 'wiki.valid.tokens')
+    test_sentences = _read_wiki(data_dir, 'wiki.test.tokens')
+    if 'Sentence Permutation' in reconstruct_ways:
+        pass
+    train_tokens, val_tokens, test_tokens, BPE = BPE_Encoding(train_sentences, val_sentences, test_sentences, num_merge)
+    return
